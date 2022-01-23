3b27605a4f90fee5e094e408a01f62ce6fe4d3bf
Hassan Habib <hassanhabib@live.com>
2022-01-23 07:55:31 -0800

    BROKERS: Initialize Storage Broker

---
+++ CulDeSacApi/Brokers/Storages/StorageBroker.cs
@@ -0,0 +1,25 @@
+﻿using EFxceptions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace CulDeSacApi.Brokers.Storages
+{
+    public class StorageBroker : EFxceptionsContext, IStorageBroker
+    {
+        private readonly IConfiguration configuration;
+
+        public StorageBroker(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            this.Database.Migrate();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString =
+                this.configuration.GetConnectionString("DefaultConnection");
+
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
