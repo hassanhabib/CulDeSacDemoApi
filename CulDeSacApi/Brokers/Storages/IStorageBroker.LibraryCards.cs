286b45df2437bf8913b01447e5cc5a0edae27664
Hassan Habib <hassanhabib@live.com>
2022-02-20 18:22:29 -0800

    BROKERS: Insert Library Card

--- CulDeSacApi/Brokers/Storages/IStorageBroker.LibraryCards.cs
+++ CulDeSacApi/Brokers/Storages/IStorageBroker.LibraryCards.cs
@@ -1,6 +1,10 @@
-﻿namespace CulDeSacApi.Brokers.Storages
+﻿using System.Threading.Tasks;
+using CulDeSacApi.Models.LibraryCards;
+
+namespace CulDeSacApi.Brokers.Storages
 {
     public partial interface IStorageBroker
     {
+        ValueTask<LibraryCard> InsertLibraryCardAsync(LibraryCard libraryCard);
     }
 }
--- CulDeSacApi/Brokers/Storages/StorageBroker.LibraryCards.cs
+++ CulDeSacApi/Brokers/Storages/StorageBroker.LibraryCards.cs
@@ -1,10 +1,24 @@
-﻿using CulDeSacApi.Models.LibraryCards;
+﻿using System.Threading.Tasks;
+using CulDeSacApi.Models.LibraryCards;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace CulDeSacApi.Brokers.Storages
 {
     public partial class StorageBroker
     {
         public DbSet<LibraryCard> LibraryCards { get; set; }
+
+        public async ValueTask<LibraryCard> InsertLibraryCardAsync(LibraryCard libraryCard)
+        {
+            using var broker = new StorageBroker(this.configuration);
+
+            EntityEntry<LibraryCard> libraryCardEntityEntry =
+                await broker.LibraryCards.AddAsync(libraryCard);
+
+            await broker.SaveChangesAsync();
+
+            return libraryCardEntityEntry.Entity;
+        }
     }
 }
