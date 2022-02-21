71fb3b7270aaeaadd4f06d71984a20d61adc3186
Hassan Habib <hassanhabib@live.com>
2022-02-20 18:27:16 -0800

    ShouldAddLibraryCardAsync -> FAIL

---
+++ CulDeSacApi/Services/Foundations/LibraryCards/LibraryCardService.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using CulDeSacApi.Brokers.Storages;
+using CulDeSacApi.Models.LibraryCards;
+
+namespace CulDeSacApi.Services.Foundations.LibraryCards
+{
+    public class LibraryCardService : ILibraryCardService
+    {
+        private readonly IStorageBroker storageBroker;
+
+        public LibraryCardService(IStorageBroker storageBroker) =>
+            this.storageBroker = storageBroker;
+
+        public ValueTask<LibraryCard> AddLibraryCardAsync(LibraryCard libraryCard)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
