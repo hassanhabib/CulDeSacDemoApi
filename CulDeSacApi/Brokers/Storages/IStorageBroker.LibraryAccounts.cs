c55b5861e2cd3839dd50e92119044c82f2ec7a80
Hassan Habib <hassanhabib@live.com>
2022-02-20 17:55:02 -0800

    BROKERS: Insert Library Account

--- CulDeSacApi/Brokers/Storages/IStorageBroker.LibraryAccounts.cs
+++ CulDeSacApi/Brokers/Storages/IStorageBroker.LibraryAccounts.cs
@@ -1,6 +1,10 @@
-﻿namespace CulDeSacApi.Brokers.Storages
+﻿using System.Threading.Tasks;
+using CulDeSacApi.Models.LibraryAccounts;
+
+namespace CulDeSacApi.Brokers.Storages
 {
     public partial interface IStorageBroker
     {
+        ValueTask<LibraryAccount> InsertLibraryAccountAsync(LibraryAccount libraryAccount);
     }
 }
